fd4284ecd3d8da4dbb9ae479f41d632a9de91046
Maximilian Lanski <79193617+SirMPixx@users.noreply.github.com>
2021-04-25 17:45:38 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,29 @@
+Config = {}
+Config.Locale = 'de'
+
+Config.DrawDistance = 100.0
+Config.MarkerSize   = {x = 1.5, y = 1.5, z = 1.0}
+Config.MarkerColor  = {r = 64, g = 235, b = 52}
+Config.MarkerType   = 27
+
+Config.Shops = {
+	--------Klamottenläden---------------------
+	vector3(-709.56,-153.93,36.50), --Ponson boys luxury
+	vector3(-1459.19,-240.52,48.85), --ponsonboys rockford hills
+    vector3(-1180.88,-764.13,16.40), --suburban little seoul 
+    vector3(-820.02,-1067.23,10.40),  --binco am kanal
+    vector3(71.15,-1387.56,28.40),   --binco groove
+    vector3(429.75,-811.59,28.55),   --binco am pd
+    vector3(236.37,753.26,199.20),    --coulson villa
+    vector3(200.49,-872.89,29.75),   --ponsonboys MP
+    vector3(-156.57,-296.50,38.75),  --ponsonboys tuning garage
+    vector3(117.57,-234.08,53.60),   --suburban burton
+    vector3(-3179.36,1034.06,19.90), --suburban west highway
+    vector3(-1100.05,2717.42,18.15), --binco military
+    vector3(617.60,2775.18,41.10),  --suburban harmony 1
+    vector3(1202.01,2714.38,37.25), --binco harmony
+    vector3(1698.68,4817.96,41.10), --binco grapeseed
+    vector3(3.65,6505.64,30.90),    --binco paleto bay
+    vector3(-441.72,6002.24,30.75), --sheriff paleto
+
+}       
---
+++ server/main.lua
@@ -0,0 +1,34 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('wardrobe:saveOutfit')
+AddEventHandler('wardrobe:saveOutfit', function(label, skin)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_datastore:getDataStore', 'property', xPlayer.identifier, function(store)
+		local dressing = store.get('dressing')
+
+		if dressing == nil then
+			dressing = {}
+		end
+
+		table.insert(dressing, {
+			label = label,
+			skin  = skin
+		})
+
+		store.set('dressing', dressing)
+	end)
+end)
+
+ESX.RegisterServerCallback('wardrobe:checkPropertyDataStore', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local foundStore = false
+
+	TriggerEvent('esx_datastore:getDataStore', 'property', xPlayer.identifier, function(store)
+		foundStore = true
+	end)
+
+	cb(foundStore)
+end)
